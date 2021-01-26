68a39791a1fd8c13dc02cb1e2689b24642bfedab
Arthur <arthur.oldrati@gmail.com>
2021-01-26 22:40:39 +0100

    IA Progress
    class enemy done, class troll in progress
    scene ready for test

---
+++ Assets/IA.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int const LookAtDistance;
+    int const MoveSpeed;
+
+    void Start()
+    {
+        attackTime = Time.time
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Object enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace classEnemy
+{
+    public class Enemy
+    {
+        protected target : Transform;
+        protected int ViewDistance;
+        protected int attackRange;
+        protected int moveSpeed;
+        protected int attack_delay;
+        protected int attack_dammage;
+        protected int health;
+        protected int KbForces;
+        protected int fightingRange=;
+        private var MoveDirection : Vector3 = Vector3.zero;
+        protected var controller = : CharacterController;
+        protected var attackTime;
+        protected int gravity = 20;
+        public abstract void attack();
+        public abstract void chase();
+        public abstract void fighting();
+    }
+    public class Troll : Enemy
+    {
+        public Troll(int ViewDistance,int attackRange,int moveSpeed,int attack_delay,int attack_dammage,int health,int KbForces)
+        {
+            this.ViewDistance = ViewDistance;
+            this.attackRange = attackRange;
+            this.moveSpeed = moveSpeed;
+            this.attack_delay = attack_delay;
+            this.attack_dammage = attack_dammage;
+            this.health = health;
+        }
+        public override void chase()
+        {
+            animation.Play("attack");
+            animation.["attack"].speed = 1;
+            moveDirection = transform.forward*moveSpeed;
+            moveDirection.y -= gravity * Time.deltaTime;
+            controller.Move(moveDirection * Time.deltaTime);
+        }
+        public override void fighting()
+        {
+            throw new System.NotImplementedException();
+        }
+        public override void attack()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
