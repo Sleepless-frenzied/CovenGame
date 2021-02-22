314d219990f28a6ab88736d21e193edfed6f3b2d
Jolan <jolan.ducluzeaud@epita.fr>
2021-02-22 23:32:04 +0100

    Fusion des map et player
    ^

---
+++ MAP and Player scene/Player/Camera_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Controller : MonoBehaviour
+{
+
+    public Transform Player;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {    
+      this.transform.position = (Player.position + offset);
+    }
+}
