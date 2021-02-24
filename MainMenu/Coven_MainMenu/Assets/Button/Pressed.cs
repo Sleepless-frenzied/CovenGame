ae70555500c1a272eca9ffe4d5ab9714ad76c0a3
Xyphes <willy.somkhit@epita.fr>
2021-02-24 22:07:15 +0100

    Ajout Main Menu etc
    Ajout Main menu + Option menu + credits

---
+++ MainMenu/Coven_MainMenu/Assets/Button/Pressed.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pressed : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> menus = new List<GameObject>();
+    
+    public void Play()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void OpenPage(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void SwitchMenu(string obj)
+    {
+        foreach (var Go in menus)
+        {
+            Go.SetActive(Go.name==obj);
+        }
+    }
+
+    /*public void Test(int index)
+    {
+        for (int i = 0 ; i < menus.Count;i++)
+            menus[i].SetActive((i==index));
+    }*/ 
+}
