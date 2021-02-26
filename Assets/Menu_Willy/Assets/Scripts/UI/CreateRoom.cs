dd502fad6fdf9ee0d15dfdfef3c0a5bebb9f9125
Yanis Yang <yanis.yang@epita.fr>
2021-02-27 00:43:54 +0100

    Ajout et modification des scripts de connections des rooms

--- Assets/Menu_Willy/Assets/Scripts/UI/CreateRoom.cs
+++ Assets/Menu_Willy/Assets/Scripts/UI/CreateRoom.cs
@@ -4,23 +4,41 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Realtime;
 using Photon.Pun;
+using TMPro;
 public class CreateRoom : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    private Text _roomName;
+    private TMP_InputField _roomName;
+
+    public Toggle t2; // load toogle buttons
+    public Toggle t3;
+    public Toggle t4;
 
     public void OnClick_CreateRoom()
     {
-        //Create Room -> Erreur si la room existe
         //JoinOrCreateRoom -> Join la room si déjà existente
+
         if (!PhotonNetwork.IsConnected)
             return;
-        RoomOptions options = new RoomOptions();
+        RoomOptions options = new RoomOptions(); // création options
         options.IsVisible = true;
-        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default);
+
+        if (t2.isOn) // selection le nbr de joueur pour la room
+        {
+            options.MaxPlayers = 2;
+        }
+        if (t3.isOn)
+        {
+            options.MaxPlayers = 3;
+        }
+        if (t4.isOn)
+        {
+            options.MaxPlayers = 4;
+        }
+        PhotonNetwork.JoinOrCreateRoom(_roomName.text, options, TypedLobby.Default); // créer la room
     }
 
-    public override void OnCreatedRoom()
+    public override void OnCreatedRoom() // debug
     {
         Debug.Log("Room created succesfully.", this);
     }
