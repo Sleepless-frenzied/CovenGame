d9d3b08cac29215fcac1683df32abd3fc04245bc
Yanis Yang <yanis.yang@epita.fr>
2021-04-06 23:03:13 +0200

    Menage & sort

---
+++ Assets/Menu/Sounds/Sounds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sounds
+{
+    public string name;
+    
+    public AudioClip clip;
+
+    [Range(0f,1f)]
+    public float volume;
+    [Range(0f,1f)]
+    public float pitch;
+
+    public bool loop;
+    
+    [HideInInspector]
+    public AudioSource source;
+}
