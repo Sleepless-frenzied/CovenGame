35ea7f94e2a25d58a8722a922e2b300db7e00b7f
Jolan <jolan.ducluzeaud@epita.fr>
2021-04-10 16:23:30 +0200

    Update git JOJO

---
+++ Assets/Scripts/Scripts_UI/UI/RoomListing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Photon.Realtime;
+using System;
+
+[Serializable]
+public class RoomListing : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text _text;
+
+    public RoomInfo RoomInfo { get; private set; }
+
+    public void SetRoomInfo(RoomInfo roominfo)
+    {
+        RoomInfo = roominfo;
+        _text.text = roominfo.Name + " / " + roominfo.MaxPlayers;
+    }
+
+}
