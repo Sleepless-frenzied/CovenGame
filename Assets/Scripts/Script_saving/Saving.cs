19954a643d977f4d38e5c37687a13d66c4fb41e4
Yanis Yang <yanis.yang@epita.fr>
2021-06-23 16:48:58 +0200

    saving started

---
+++ Assets/Scripts/Script_saving/Saving.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Realtime;
+using UnityEngine;
+
+public class Saving : MonoBehaviour
+{
+	public void Serialize(Player player)
+	{
+		var _name = player.NickName;
+		var _properties = player.CustomProperties;
+		
+	}
+}
