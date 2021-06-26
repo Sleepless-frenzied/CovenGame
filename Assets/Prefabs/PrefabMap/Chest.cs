5e4ca064e8300506d05cfce92098669eec2ced46
Jolan <jolan.ducluzeaud@epita.fr>
2021-06-27 00:38:21 +0200

    mise à niveau, nouveaux skin de perso

--- Assets/Prefabs/PrefabMap/Chest.cs
+++ Assets/Prefabs/PrefabMap/Chest.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Chest : Interactable
 {
     public Mesh mesh;
+    public GameObject bagWeapon;
+    public GameObject bagArmor;
+    public GameObject bagItem;
 
     public override void Interact()
     {
         GetComponent<MeshFilter>().mesh = mesh;
+        Destroy(transform.GetChild(0).gameObject);
+        if (Random.Range(0,10) >= 3)
+            Instantiate(bagWeapon, transform);
+        if (Random.Range(0,10) >= 3)
+            Instantiate(bagArmor, transform);
+        if (Random.Range(0,10) >= 2)
+            Instantiate(bagItem, transform);
+
+
+        gameObject.layer = 9;
     }
 }
