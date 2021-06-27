45286c07fbc9bba6925a4bf7b587795f1d6235a3
Yanis Yang <yanis.yang@epita.fr>
2021-06-27 13:43:21 +0200

    Added photon view to all player model

--- Assets/Scripts/Script_saving/Saving.cs
+++ Assets/Scripts/Script_saving/Saving.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 [Serializable]
 public class Saving : MonoBehaviourPunCallbacks
 {
-	/*public void save (UnarmedCharacter player, int slot)
+	public void save (UnarmedCharacter player, int slot)
 	{
 		var damagePower = player.damagePower;
 		var armorPower = player.armorPower;
@@ -28,12 +29,44 @@ public class Saving : MonoBehaviourPunCallbacks
 				eqslot = (int) eq.equipSlot;
 			}
 		}
-	}*/
+	}
 
-	public string Savejson(UnarmedCharacter player, int saveslot)
+	public void Savejson(UnarmedCharacter player, int saveslot = 0)
 	{
+		
 		var res = JsonUtility.ToJson(player);
 		Debug.Log(res);
-		return res;
+		var path = "/save" + saveslot.ToString();
+		if (File.Exists(path))
+		{
+			throw new NotImplementedException();
+		}
+
+		using (var sw = new StreamWriter(path))
+		{
+			sw.Write(res);
+			Debug.Log("");
+			sw.Close();
+		}
 	}
+
+	public UnarmedCharacter LoadJson(string path, int slot)
+	{
+		var file = path + slot.ToString();
+		
+		try
+		{
+			var reader = new StreamReader(file);
+			var json = reader.ReadToEnd();
+			var player = JsonUtility.FromJson<UnarmedCharacter>(json);
+			return player;
+		}
+		catch
+		{
+			Debug.Log("Saving.cs : Error loadjson");
+		}
+
+		return new UnarmedCharacter();
+	}
+	
 }
